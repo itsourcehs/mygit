cbc7558b200bfa545ac86c5e7f63642e3c83e37b
admin <1073473261@qq.com>
2021-05-24 09:21:57 +0800

    202105240921

--- Blog/src/main/java/com/site/blog/my/core/controller/admin/AdminController.java
+++ Blog/src/main/java/com/site/blog/my/core/controller/admin/AdminController.java
@@ -2,11 +2,15 @@ package com.site.blog.my.core.controller.admin;
 
 import com.site.blog.my.core.service.BlogService;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * @program: Blog
@@ -32,6 +36,20 @@ public class AdminController {
 
         request.setAttribute("blogCount",blogService.getTotalBlogs());
         return "admin/index";
+    }
+
+    @PostMapping(value = "/login")
+    public String login(@RequestParam("userName") String userName
+            , @RequestParam("password") String password
+            , @RequestParam("verifyCode") String verifyCode
+            , HttpSession session){
+        if(StringUtils.isEmpty(verifyCode)){
+            session.setAttribute("errorMsg","验证码不能为空");
+            return "admin/login";
+        }else {
+
+        }
+        return "1";
     }
 
 

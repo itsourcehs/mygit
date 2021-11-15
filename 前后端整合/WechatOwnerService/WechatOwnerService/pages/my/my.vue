cfeb770300c04a7a89c2766d552b071a2cc90b82
admin <1073473261@qq.com>
2021-11-15 09:17:37 +0800

    202111150917

--- 前后端整合/WechatOwnerService/WechatOwnerService/pages/my/my.vue
+++ 前后端整合/WechatOwnerService/WechatOwnerService/pages/my/my.vue
@@ -1,6 +1,44 @@
 <template>
 	<view>
 		
+		<view>
+			<view class="">
+				
+				<view class="content">
+					<text></text>
+					<text>业主信息</text>
+				</view>
+				
+				<view class="">
+					<view class="content">
+						<text></text>
+						<text>我的房屋</text>
+					</view>
+				</view>
+				
+				<view class="">
+					<view class="content">
+						<text></text>
+						<text>商城订单</text>
+					</view>
+				</view>
+				
+				<view class="">
+					<view class="content">
+						<text></text>
+						<text>家政订单</text>
+					</view>
+				</view>
+				
+				<view class="">
+					<view class="content">
+						<text></text>
+						<text>缴费历史</text>
+					</view>
+				</view>
+				
+			</view>
+		</view>
 	</view>
 </template>
 

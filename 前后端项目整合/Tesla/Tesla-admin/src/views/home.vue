90f16bef654b82d20a1221748fa8918c7953e653
admin <1073473261@qq.com>
2022-03-03 09:24:03 +0800

    202203030923

--- 前后端项目整合/Tesla/Tesla-admin/src/views/home.vue
+++ 前后端项目整合/Tesla/Tesla-admin/src/views/home.vue
@@ -2,54 +2,7 @@
   <el-container style="height: 100%">
     <el-aside width="200px">
       <img src="@/assets/img/admin_icon.jpg" style="height: 44px;width: auto">
-      <el-menu
-        router
-        default-active="1-1"
-        class="el-menu-vertical-demo"
-        @open="handleOpen"
-        @close="handleClose"
-        background-color="#495060"
-        text-color="#fff"
-        style="border-right: none"
-        active-text-color="#ffd04b">
-
-        <el-submenu index="1">
-          <template slot="title">
-            <i class="el-icon-s-order"></i>
-            <span>瓷砖管理</span>
-          </template>
-          <el-menu-item-group>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-            <el-menu-item index="1-3">选项3</el-menu-item>
-          </el-menu-item-group>
-        </el-submenu>
-
-        <el-submenu index="2">
-          <template slot="title">
-            <i class="el-icon-user"></i>
-            <span>用户管理</span>
-          </template>
-          <el-menu-item-group>
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-            <el-menu-item index="2-3">选项3</el-menu-item>
-          </el-menu-item-group>
-        </el-submenu>
-
-        <el-submenu index="3">
-          <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>角色管理</span>
-          </template>
-          <el-menu-item-group>
-            <el-menu-item index="3-1">选项1</el-menu-item>
-            <el-menu-item index="3-2">选项2</el-menu-item>
-            <el-menu-item index="3-3">选项3</el-menu-item>
-          </el-menu-item-group>
-        </el-submenu>
-
-      </el-menu>
+      <admin-menu></admin-menu>
     </el-aside>
 
     <el-container style="width: 89%;margin-left: 200px">
@@ -78,8 +31,12 @@
 </template>
 
 <script>
+	import AdminMenu from '../components/AdminMenu.vue'; // 引用方法需要{} 引用组件不需要加{}
 	export default {
 		name: 'Home',
+		components: {
+			AdminMenu
+		},
 		data() {
 			return {
 

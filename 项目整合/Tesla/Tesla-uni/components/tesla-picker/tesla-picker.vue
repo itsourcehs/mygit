53a4d027f84f0b5cb0ff45a55842082ba49b9e4d
admin <1073473261@qq.com>
2022-03-29 09:21:59 +0800

    202203290921

--- 项目整合/Tesla/Tesla-uni/components/tesla-picker/tesla-picker.vue
+++ 项目整合/Tesla/Tesla-uni/components/tesla-picker/tesla-picker.vue
@@ -1,16 +1,49 @@
 <template>
 	<view>
-		
+		<picker class="picker" @change="onChange" :value="index" :range="optionList">
+			<tesla-input :writable="false" required :label="title" :val="value">
+				<image class="icon-down" src="@/static/images/ic_arrow_down.svg"></image>
+			</tesla-input>
+		</picker>
 	</view>
 </template>
 
 <script>
 	export default {
 		name:"tesla-picker",
+		props: {
+			 title: {
+				type: String,
+				value: ''
+			},
+			labelKey: {
+				type: String,
+				value: ''
+			},
+			defaultValue: {
+				type: String | Number,
+				value: '',
+				observer(newValue, oldValue) {
+					this.initValue(newValue)
+				}
+			},
+			options: {
+				type: Array,
+				value: [],
+				observer() {
+					this.buildOptionList()
+				}
+			}
+		},
 		data() {
 			return {
-				
+				value: '',
+				index: 0,
+				optionList: []
 			};
+		},
+		methods: {
+			
 		}
 	}
 </script>
